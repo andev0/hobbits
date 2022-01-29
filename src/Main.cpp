a8071d3a70da5fe85973a3304c630b3bedb43630
andev0 <fejalex@gmail.com>
2022-01-29 15:59:21 +0200

    Added base Raylib code

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,21 @@
+#include <raylib.h>
+
 int main()
 {
-    
+    const Vector2 screenSize {360, 640};
+
+    InitWindow(screenSize.x, screenSize.y, "Hobbits");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        {}
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
