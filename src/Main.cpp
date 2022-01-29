db214eb4bee98958f568186a083e11c50671e6ff
andev0 <fejalex@gmail.com>
2022-01-29 23:21:15 +0200

    Created Text class

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,5 @@
 #include "Engine/GraphicsObjects/Rectangle.h"
+#include "Engine/GraphicsObjects/Text.h"
 
 int main()
 {
@@ -7,6 +8,7 @@ int main()
     InitWindow(screenSize.x, screenSize.y, "Hobbits");
 
     hbt::Rectangle rect(nullptr, {0, 0}, screenSize, DARKGRAY);
+    hbt::Text text(&rect, {5, 5}, "Hello World!", 40.0f, RAYWHITE, "Resources/Fonts/Comfortaa/Regular.ttf");
 
     while (!WindowShouldClose())
     {
@@ -14,6 +16,7 @@ int main()
 
         {
             rect.draw();
+            text.draw();
         }
 
         EndDrawing();
